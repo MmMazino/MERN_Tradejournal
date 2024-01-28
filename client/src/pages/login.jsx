0e21566e5727313bc844221b51cdf9086b615ef8
MmMazino <maggiza9@gmail.com>
2024-01-28 22:42:59 +0700

    add validation from yup in page register

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const login = () => {
+  const handleLogin = ()=>{}
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-900">
       <div className="max-w-md bg-white rounded-md p-10 mx-auto w-full">
@@ -30,17 +31,17 @@ const login = () => {
                 className="w-full text-base py-2 border-b bg-white border-gray-300 focus:outline-none focus:border-indigo-500 text-black"
               ></input>
             </div>
-            <div class="flex items-center justify-between mt-4">
-              <div class="flex items-center">
+            <div className="flex items-center justify-between mt-4">
+              <div className="flex items-center">
                 <input
                   id="remember_me"
                   name="remember_me"
                   type="checkbox"
-                  class="h-4 w-4 bg-white focus:ring-indigo-400 border-gray-300 rounded"
+                  className="h-4 w-4 bg-white focus:ring-indigo-400 border-gray-300 rounded"
                 ></input>
                 <label
                   for="remember_me"
-                  class="ml-2 block text-sm text-gray-900"
+                  className="ml-2 block text-sm text-gray-900"
                 >
                   Remember me
                 </label>
@@ -49,17 +50,17 @@ const login = () => {
             <div className="mt-8">
               <button
                 type="submit"
-                class="w-full flex justify-center bg-indigo-500 text-gray-100 p-4  rounded-full tracking-wide
+                className="w-full flex justify-center bg-indigo-500 text-gray-100 p-4  rounded-full tracking-wide
                                 font-semibold  focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300"
               >
                 Sign in
               </button>
             </div>
-            <p class="flex flex-col items-center justify-center mt-10 text-center text-md text-gray-500">
+            <p className="flex flex-col items-center justify-center mt-10 text-center text-md text-gray-500">
               <span>Don't have an account?</span>
               <a
                 href={`/register`}
-                class="text-indigo-500 hover:text-indigo-500no-underline hover:underline cursor-pointer transition ease-in duration-300"
+                className="text-indigo-500 hover:text-indigo-500no-underline hover:underline cursor-pointer transition ease-in duration-300"
               >
                 Sign up
               </a>
--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,6 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import * as yup from "yup";
 
 const register = () => {
+  const userSchema = yup.object().shape({
+    firstName: yup
+      .string()
+      .min(2, "First name must be at least 2 characters")
+      .max(50, "First name cannot exceed 50 characters")
+      .required("First name is required"),
+    lastName: yup
+      .string()
+      .min(2, "Last name must be at least 2 characters")
+      .max(50, "Last name cannot exceed 50 characters")
+      .required(" name is required"),
+    email: yup.string().email("Invalid email").required("Email is required"),
+    password: yup
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .required("Password is required"),
+  });
+
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const [valid, setValid] = useState({
+    firstName: false,
+    lastName: false,
+    email: false,
+    password: false,
+  });
+
+  const handleInput = (e) => {
+    const { id, value } = e.target;
+
+    // Update the user state
+    setUser((prevUser) => ({
+      ...prevUser,
+      [id]: value,
+    }));
+
+    // Validate the updated user object
+    userSchema
+      .validateAt(id, { [id]: value })
+      .then(() => {
+        setValid((prevValid) => ({ ...prevValid, [id]: true }));
+      })
+      .catch((error) => {
+        setValid((prevValid) => ({ ...prevValid, [id]: false }));
+        console.error(error.message);
+      });
+  };
+  useEffect(() => {
+    console.log(user);
+    console.log(valid);
+  }, [handleInput]);
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-900">
       <div className="max-w-md bg-white rounded-md p-10 mx-auto w-full">
@@ -16,6 +73,8 @@ const register = () => {
               </label>
               <input
                 type="text"
+                id="firstName"
+                onChange={handleInput}
                 required
                 min={2}
                 max={50}
@@ -28,18 +87,22 @@ const register = () => {
               </label>
               <input
                 type="text"
+                id="lastName"
+                onChange={handleInput}
                 required
                 min={2}
                 max={50}
                 className="w-full text-base py-2 border-b bg-white border-gray-300 focus:outline-none focus:border-indigo-500 text-black"
               ></input>
-            </div >
+            </div>
             <div className="mt-4">
               <label className="text-sm font-bold text-gray-700 tracking-wide">
                 Email:
               </label>
               <input
                 type="text"
+                id="email"
+                onChange={handleInput}
                 required
                 className="w-full text-base py-2 border-b bg-white border-gray-300 focus:outline-none focus:border-indigo-500 text-black"
               ></input>
@@ -50,6 +113,8 @@ const register = () => {
               </label>
               <input
                 type="password"
+                id="password"
+                onChange={handleInput}
                 required
                 className="w-full text-base py-2 border-b bg-white border-gray-300 focus:outline-none focus:border-indigo-500 text-black"
               ></input>
@@ -57,17 +122,17 @@ const register = () => {
             <div className="mt-4">
               <button
                 type="submit"
-                class="w-full flex justify-center bg-indigo-500 text-gray-100 p-4  rounded-full tracking-wide
+                className="w-full flex justify-center bg-indigo-500 text-gray-100 p-4  rounded-full tracking-wide
                                 font-semibold  focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300"
               >
                 Register
               </button>
             </div>
-            <p class="flex flex-col items-center justify-center mt-10 text-center text-md text-gray-500">
+            <p className="flex flex-col items-center justify-center mt-10 text-center text-md text-gray-500">
               <span>Do have an account?</span>
               <a
                 href={`/login`}
-                class="text-indigo-500 hover:text-indigo-500no-underline hover:underline cursor-pointer transition ease-in duration-300"
+                className="text-indigo-500 hover:text-indigo-500no-underline hover:underline cursor-pointer transition ease-in duration-300"
               >
                 Sign in
               </a>
@@ -76,7 +141,7 @@ const register = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default register
+export default register;
